dd8ae80ab58184606311b9c902ebd192d86e81f5
binhex <megalith01@gmail.com>
2019-11-18 15:13:30 +0000

    change path for novnc

--- build/root/install.sh
+++ build/root/install.sh
@@ -57,7 +57,7 @@ rm -f '/etc/ssmtp/ssmtp.conf'
 ###
 
 # overwrite novnc 16x16 icon with application specific 16x16 icon (used by bookmarks and favorites)
-cp /home/nobody/novnc-16x16.png /usr/share/novnc/app/images/icons/
+cp /home/nobody/novnc-16x16.png /usr/share/webapps/novnc/app/images/icons/
 
 cat <<'EOF' > /tmp/startcmd_heredoc
 # launch xfce4-terminal (we cannot simply call /usr/bin/xfce4-terminal otherwise it wont run on startup)
@@ -76,7 +76,7 @@ rm /tmp/startcmd_heredoc
 ####
 
 # define comma separated list of paths
-install_paths="/tmp,/usr/share/themes,/home/nobody,/usr/share/novnc,/usr/share/applications,/etc/xdg"
+install_paths="/tmp,/usr/share/themes,/home/nobody,/usr/share/webapps/novnc,/usr/share/applications,/etc/xdg"
 
 # split comma separated string into list for install paths
 IFS=',' read -ra install_paths_list <<< "${install_paths}"
